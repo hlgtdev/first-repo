3a29d43f2e99885129b3c70d1a27fa5f35bcdf9d
hlgtdev <>
2022-08-23 15:22:42 +0200

    Ajout de la config de l'environnement

---
+++ 0-divers/config-example-env.sh
@@ -0,0 +1,9 @@
+export XMPG_HOME=~/projets/UTLDEV-prog-par-example/first-repo
+
+sudo aptitude install default-jdk
+sudo aptitude install graphviz
+sudo aptitude install python3-pyqt5
+
+sudo pip3 install readchar
+
+sudo ln -s $XMPG_HOME/xmprog/xmprog-runner.py /usr/local/bin/xmpg
