b1f4b08d71cf314f5e33758aa3eb86132bdddf7a
Debian Live user <user@debian-BULLSEYE-live-builder-AMD64>
2022-08-24 10:42:06 +0000

    Mise au point de la config de l'environnement

--- 0-divers/config-example-env.sh
+++ 0-divers/config-example-env.sh
@@ -1,9 +1,31 @@
 export XMPG_HOME=~/projets/UTLDEV-prog-par-example/first-repo
 
+if [[ ! -d $XMPG_HOME ]];then
+    export XMPG_HOME=/media/user/3ADA3A76DA3A2F0F/Users/jla/dev/first-repo
+fi
+
+echo $XMPG_HOME
+
+sudo apt-get install aptitude
+
+sudo aptitude install git
 sudo aptitude install default-jdk
 sudo aptitude install graphviz
 sudo aptitude install python3-pyqt5
+sudo aptitude install python3-pip
 
 sudo pip3 install readchar
+sudo pip3 install pyyaml
 
+aptitude show atom
+
+if [[ $? -eq 0 ]];then
+    if [[ ! -f ../../atom-amd64.deb ]];then
+        wget https://github.com/atom/atom/releases/download/v1.54.0/atom-amd64.deb -O ../../atom-amd64.deb
+    fi
+    
+    sudo gdebi ../../atom-amd64.deb
+fi
+
+sudo rm -f /usr/local/bin/xmpg
 sudo ln -s $XMPG_HOME/xmprog/xmprog-runner.py /usr/local/bin/xmpg
