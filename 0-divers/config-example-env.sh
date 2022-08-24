b57b563b6deee6c259bb5473faf35fc085b2bc22
Debian Live user <user@debian-BULLSEYE-live-builder-AMD64>
2022-08-24 10:49:32 +0000

    Config env

--- 0-divers/config-example-env.sh
+++ 0-divers/config-example-env.sh
@@ -1,12 +1,20 @@
 export XMPG_HOME=~/projets/UTLDEV-prog-par-example/first-repo
 
+bInstallClavierVirtuel=0
+
 if [[ ! -d $XMPG_HOME ]];then
     export XMPG_HOME=/media/user/3ADA3A76DA3A2F0F/Users/jla/dev/first-repo
+    bInstallClavierVirtuel=1
 fi
 
 echo $XMPG_HOME
 
+sudo apt-get update
 sudo apt-get install aptitude
+
+if [[ $bInstallClavierVirtuel -eq 1 ]];then
+    sudo aptitude install onboard
+fi
 
 sudo aptitude install git
 sudo aptitude install default-jdk
@@ -19,7 +27,7 @@ sudo pip3 install pyyaml
 
 aptitude show atom
 
-if [[ $? -eq 0 ]];then
+if [[ $? -gt 0 ]];then
     if [[ ! -f ../../atom-amd64.deb ]];then
         wget https://github.com/atom/atom/releases/download/v1.54.0/atom-amd64.deb -O ../../atom-amd64.deb
     fi
